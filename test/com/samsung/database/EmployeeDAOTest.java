9a93bfaf3862ac6afdd8cf361dac9b04c22a70f4
leeyounho <thestarrises@naver.com>
2022-03-21 10:01:52 +0900

    [Add] EmployeeTable에서 Key를 Modify하는 경우에 대한 테스트 함수 작성

--- test/com/samsung/database/EmployeeDAOTest.java
+++ test/com/samsung/database/EmployeeDAOTest.java
@@ -2,9 +2,9 @@ package com.samsung.database;
 
 import com.samsung.constants.CareerLevel;
 import com.samsung.constants.Certi;
-import com.samsung.database.table.EmployeeTable;
 import com.samsung.employee.Employee;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
@@ -14,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeDAOTest {
     private static EmployeeDAO employeeDAO;
 
-    @BeforeAll
-    public static void beforeAll() {
+    @BeforeEach
+    public void beforeEach() {
         employeeDAO = new EmployeeDAO();
         employeeDAO.add(new Employee("18050301", "KYUMOK KIM", CareerLevel.CL2, "010-9777-6055", "19980906", Certi.PRO));
         employeeDAO.add(new Employee("12343254", "YOUNHO LEE", CareerLevel.CL3, "010-1234-6055", "20210410", Certi.ADV));
@@ -27,6 +27,11 @@ class EmployeeDAOTest {
         employeeDAO.add(new Employee("34563645", "MYUNGSU PARK", CareerLevel.CL3, "010-8233-5833", "20100815", Certi.PRO));
         employeeDAO.add(new Employee("54687253", "HONGCHUL NO", CareerLevel.CL2, "010-5790-2364", "20080727", Certi.ADV));
         employeeDAO.add(new Employee("86907892", "HA HA", CareerLevel.CL2, "010-0078-5257", "19980906", Certi.ADV));
+    }
+
+    @AfterEach
+    public void afterEach(){
+        employeeDAO.deleteAll();
     }
 
     public void printAll(String head, Set<Employee> employees) {
@@ -51,7 +56,7 @@ class EmployeeDAOTest {
     @Test
     public void searchByName() {
         Employee condition = new Employee();
-        condition.setName("HYUNGDON LEE");
+        condition.setName("HYUNGDON JUNG");
 
         for (Employee employeeResult : employeeDAO.search(condition)) {
             System.out.println(employeeResult.toString());
@@ -186,12 +191,15 @@ class EmployeeDAOTest {
         Set<Employee> queryResult;
 
         queryResult = employeeDAO.search(addCondition);
+        int sizeBeforeAdd = queryResult.size();
         printAll("before add", queryResult);
 
         employeeDAO.add(addCondition);
         queryResult = employeeDAO.search(addCondition);
+        int sizeAfterAdd = queryResult.size();
         printAll("after add", queryResult);
 
+        assertEquals(sizeBeforeAdd, sizeAfterAdd - 1);
         assertTrue(queryResult.contains(addCondition));
     }
 
@@ -211,12 +219,276 @@ class EmployeeDAOTest {
     }
 
     @Test
-    void modifyTest() {
-//        Employee modifyCondition = new Employee("23423434", "JAEYOUNG KIM", CareerLevel.CL2, "010-4543-5687", "20210850", Certi.ADV);
-//        Employee modifyCondition = new Employee("23423434", "JAEYOUNG KIM", CareerLevel.CL2, "010-4543-5687", "20210850", Certi.ADV);
-//        employeeDAO.modify(modifyCondition);
-//
-//        Set<Employee> queryResult = employeeDAO.search(addCondition);
-//        assertTrue(!queryResult.contains(addCondition));
+    void modifyEmployeeNumber() {
+        Employee searchCondition = new Employee();
+        searchCondition.setEmployeeNumber("23423434");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setEmployeeNumber("00003434");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getEmployeeNumber(), "23423434");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyName() {
+        Employee searchCondition = new Employee();
+        searchCondition.setName("WONKYUNG YUN");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setName("MODIFIED NAME");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getName(), "WONKYUNG YUN");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyFirstName() {
+        Employee searchCondition = new Employee();
+        searchCondition.setFirstName("JAESUCK");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setFirstName("MODIFIED");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getFirstName(), "JAESUCK");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyLastName() {
+        Employee searchCondition = new Employee();
+        searchCondition.setLastName("LEE");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setLastName("MODIFIED");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getLastName(), "LEE");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyPhoneNumber() {
+        Employee searchCondition = new Employee();
+        searchCondition.setPhoneNumber("010-4543-5687");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setPhoneNumber("000-0000-0000");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getPhoneNumber(), "010-4543-5687");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyMiddleDigitOfPhoneNumber() {
+        Employee searchCondition = new Employee();
+        searchCondition.setMiddleDigitOfPhoneNumber("4543");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setMiddleDigitOfPhoneNumber("0000");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getMiddleDigitOfPhoneNumber(), "4543");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyLast4DigitOfPhoneNumber() {
+        Employee searchCondition = new Employee();
+        searchCondition.setLast4DigitOfPhoneNumber("5687");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setLast4DigitOfPhoneNumber("0000");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getLast4DigitOfPhoneNumber(), "5687");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyBirthDay() {
+        Employee searchCondition = new Employee();
+        searchCondition.setBirthDay("19980906");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setBirthDay("00000000");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getBirthDay(), "19980906");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyYearOfBirth() {
+        Employee searchCondition = new Employee();
+        searchCondition.setYearOfBirth("1980");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setYearOfBirth("0000");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getYearOfBirth(), "1980");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyMonthOfBirth() {
+        Employee searchCondition = new Employee();
+        searchCondition.setMonthOfBirth("09");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setMonthOfBirth("00");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getMonthOfBirth(), "09");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyDayOfBirth() {
+        Employee searchCondition = new Employee();
+        searchCondition.setDayOfBirth("01");
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setDayOfBirth("00");
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getDayOfBirth(), "01");
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyCareerLevel() {
+        Employee searchCondition = new Employee();
+        searchCondition.setCareerLevel(CareerLevel.CL3);
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setName("DUMMY DUM");
+        modifyCondition.setCareerLevel(CareerLevel.CL2);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getCareerLevel(), CareerLevel.CL3);
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
+    }
+
+    @Test
+    void modifyCerti() {
+        Employee searchCondition = new Employee();
+        searchCondition.setCerti(Certi.PRO);
+
+        Employee modifyCondition = new Employee();
+        modifyCondition.setCerti(Certi.ADV);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            assertEquals(employee.getCerti(), Certi.PRO);
+        }
+
+        employeeDAO.modify(searchCondition, modifyCondition);
+
+        for (Employee employee : employeeDAO.search(searchCondition)) {
+            System.out.println(employee.toString());
+            fail();
+        }
     }
 }
