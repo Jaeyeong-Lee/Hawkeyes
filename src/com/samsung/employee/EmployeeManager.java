1198022a2fc1923679ecc17b2b4a2886c4d35b4f
leeyounho <thestarrises@naver.com>
2022-03-21 09:34:29 +0900

    [Add] Employee 객체 2개가 있을 경우 하나로 병합하는 메소드 추가

--- src/com/samsung/employee/EmployeeManager.java
+++ src/com/samsung/employee/EmployeeManager.java
@@ -1,7 +1,6 @@
 package com.samsung.employee;
 
 import com.samsung.command.Command;
-import com.samsung.database.PersistentDAO;
 import com.samsung.iomanager.IOManager;
 
 import java.util.List;
@@ -11,6 +10,24 @@ public class EmployeeManager {
     private IOManager<String> fileIOManager;
     private List<Command> commandList;
 
+    public Employee overWrite(Employee asIsEmployee, Employee toBeEmployee) {
+        Employee employee = new Employee();
 
+        employee.setEmployeeNumber(toBeEmployee.getEmployeeNumber() != null ? toBeEmployee.getEmployeeNumber() : asIsEmployee.getEmployeeNumber());
+        employee.setName(toBeEmployee.getName() != null ? toBeEmployee.getName() : asIsEmployee.getName());
+        employee.setFirstName(toBeEmployee.getFirstName() != null ? toBeEmployee.getFirstName() : asIsEmployee.getFirstName());
+        employee.setLastName(toBeEmployee.getLastName() != null ? toBeEmployee.getLastName() : asIsEmployee.getLastName());
+        employee.setPhoneNumber(toBeEmployee.getPhoneNumber() != null ? toBeEmployee.getPhoneNumber() : asIsEmployee.getPhoneNumber());
+        employee.setMiddleDigitOfPhoneNumber(toBeEmployee.getMiddleDigitOfPhoneNumber() != null ? toBeEmployee.getMiddleDigitOfPhoneNumber() : asIsEmployee.getMiddleDigitOfPhoneNumber());
+        employee.setLast4DigitOfPhoneNumber(toBeEmployee.getLast4DigitOfPhoneNumber() != null ? toBeEmployee.getLast4DigitOfPhoneNumber() : asIsEmployee.getLast4DigitOfPhoneNumber());
+        employee.setBirthDay(toBeEmployee.getBirthDay() != null ? toBeEmployee.getBirthDay() : asIsEmployee.getBirthDay());
+        employee.setYearOfBirth(toBeEmployee.getYearOfBirth() != null ? toBeEmployee.getYearOfBirth() : asIsEmployee.getYearOfBirth());
+        employee.setMonthOfBirth(toBeEmployee.getMonthOfBirth() != null ? toBeEmployee.getMonthOfBirth() : asIsEmployee.getMonthOfBirth());
+        employee.setDayOfBirth(toBeEmployee.getDayOfBirth() != null ? toBeEmployee.getDayOfBirth() : asIsEmployee.getDayOfBirth());
+        employee.setCareerLevel(toBeEmployee.getCareerLevel() != null ? toBeEmployee.getCareerLevel() : asIsEmployee.getCareerLevel());
+        employee.setCerti(toBeEmployee.getCerti() != null ? toBeEmployee.getCerti() : asIsEmployee.getCerti());
+
+        return employee;
+    }
 
 }
