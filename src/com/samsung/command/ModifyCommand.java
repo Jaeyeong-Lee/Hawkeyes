70168374651dace96ab6121f7075acf1fa933bd0
leeyounho <thestarrises@naver.com>
2022-03-22 10:58:14 +0900

    [Refactoring] 불필요한 local 변수 제거 generic 문자 E -> T로 수정(다른 코드와 통일)

--- src/com/samsung/command/ModifyCommand.java
+++ src/com/samsung/command/ModifyCommand.java
@@ -5,9 +5,10 @@ import com.samsung.option.CommandOption;
 
 import java.util.Set;
 
-public class ModifyCommand<E> extends Command<Set<Employee>> {
+public class ModifyCommand<T> extends Command<Set<Employee>> {
 
-    public ModifyCommand() {};
+    public ModifyCommand() {
+    }
 
     public ModifyCommand(CommandOption commandOption) {
         super(commandOption);
@@ -15,14 +16,7 @@ public class ModifyCommand<E> extends Command<Set<Employee>> {
 
     @Override
     public Set<Employee> execute() {
-        // 1. CommandOption -> Employee 객체로 convert
-        Employee asIsEmployee = commandOption.convertSearchOptionToEmployee();
-        Employee toBeEmployee = commandOption.convertModifyOptionToEmployee();
-
-        // 2. EmployeeDAO.delete() 수행
-        Set<Employee> returnEmp = employeeDAO.modify(asIsEmployee, toBeEmployee);
-
-        return returnEmp;
+        return employeeDAO.modify(commandOption.convertSearchOptionToEmployee(), commandOption.convertModifyOptionToEmployee());
     }
 
     @Override
