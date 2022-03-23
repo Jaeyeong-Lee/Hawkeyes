74f6ea4a36428647c81a68dc0ee62c703e4312d9
Jay <w22ee1@gmail.com>
2022-03-23 10:38:27 +0900

    [Refactoring] constant 추가

--- src/com/samsung/command/CommandFactory.java
+++ src/com/samsung/command/CommandFactory.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 
 public class CommandFactory {
-    
+
     public Command getCommand(String line) {
         try {
             CommandAttr commandAttr = new CommandAttr();
